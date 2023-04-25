f367efae8f11b0d9ba13724e7cb6bcbcbd72769e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-25 13:46:32 -0300

    primeiro commit

---
+++ POO/script.js
@@ -0,0 +1,90 @@
+let carro = {
+    marca : "chevrolet",
+    modelo : "onix",
+    ano : 2018,   
+    preco : 45000,
+    motor : {
+        cilindradas : 1.4,
+        potencia : 100
+    },
+    acelerar: function(){
+        console.log("acelerando...")
+    }
+};
+
+console.log("tempo de fabricação:", (2023-carro.ano));
+
+let pessoa = {
+    nome : "joaquina",
+    anonascimento : 1980,
+    mostraIdade : function(){
+        console.log("sua idade é", 2023 - this.anonascimento) // o this informa que este atributo esta dentro do proprio objeto
+    }
+};
+
+// desafio
+
+let transfomer = {
+    nome : "starscream",
+    altMode : "F-16",
+    facção : "decepticon",
+    status : "ia",
+    patente : "segundo em comando",
+    tamanho : "8 metros",
+    continuidade : "G1"
+};
+
+let mobilesuit = {
+    nome : "rx-78",
+    status : "kia",
+    facção : "federação da terra",
+    tamanho : "18 metros",
+    armas : {
+        arma1 : "Beam Saber",
+        arma2 : "Beam Javelin",
+        arma3 : "BOWA*XBR-M-79-07G Beam Rifle",
+        arma4 : "BLASH*XHB-L-03/N-STD Hyper Bazooka",
+        arma5 : "RX*M-Sh-008/S-01025 Shield",
+        arma6 : "Super Napalm",
+        arma7 : "Gundam Hammer",
+        arma8 : "Hyper Hammer"
+    },
+    continuidade : "UC"
+};
+
+carro.acelerar()
+
+pessoa.mostraIdade()
+
+// desafio 2
+
+let avião = {
+    ligado : false,
+    tipo : "boinng",
+    velocidadeMaxima: 200,
+    velocidade : 0,
+    qtDePassageiros : 300,
+
+    acelarando : function(){
+        if(this.ligado && this.velocidade<=this.velocidadeMaxima){
+          this.velocidade = this.velocidade + 10
+        }
+    },
+    ligar : function(){
+        this.ligado = true
+        console.log("avião ligado ...")
+    },
+    desligar : function(){
+        this.ligado = false
+        console.log("avião desligado...")
+    }
+}
+
+avião.ligar()
+console.log("acelerando...")
+for(let i=1; i <= 20; i++){
+avião.acelarando()
+console.log("velocidade atual:", avião.velocidade, "km/h")
+}
+
+avião.desligar()
