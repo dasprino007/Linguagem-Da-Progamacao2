7a3016c87b23da6b8c549947a04964681ed4ea92
Diego Asprino <diegoasprino07@gmail.com>
2023-06-27 20:55:27 -0300

    lolja terminada

--- revpoo/lolja.js
+++ revpoo/lolja.js
@@ -9,31 +9,32 @@ let carinho = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 // variaveis
 let x = 0;
 let loja = "Loja Transatlântica";
-let slogan = "Atravessamos o que for para entregar a melhor \n qualidade até você!";
+let slogan = "Atravessamos o que for para entregar a melhor qualidade até você!";
 let i = 0;
 let o = 0;
 let op = 0;
+let total = 0;
 
-
-while(x < 1){
     console.log(loja)
     console.log(slogan)
-    let pergunta = leia("deseja continuar S/N :")
-    if(pergunta == "N" || pergunta == "n"){// se não
-        console.log("tenha um bom dia");
-        break;
+    let pergunta = leia("Deseja Comprar S/N :");
+
+    if(pergunta == "N" || pergunta == "n"){
+        console.log("Tenha um bom dia");
     }
+
+    else{
     console.log("codigo"+'\t'+"produto"+'\t'+"valor"+'\t'+"estoque")
     for(let i=0; i<codigo.length; i++){
             console.log(codigo[i]+'\t'+produto[i]+'\t'+valor[i]+'\t'+estoque[i]+'\t'+carinho[i])
     }
-        console.log("carinho de compras")
+        console.log("Carrinho de compras")
         for(let i=0; i<codigo.length; i++){
             if(carinho[i] != null && carinho[i] > 0){
                 console.log(codigo[i]+'\t'+produto[i]+'\t'+valor[i]+'\t'+estoque[i]+'\t'+carinho[i])
             }
         }
-        let cod = leia("coloque o codigo do produto :")
+        let cod = leia("Coloque o código do produto :")
         for(let i=0; i<codigo.length; i++){
             if(cod == codigo[i]){
                 console.log(codigo[i]+'\t'+produto[i]+'\t'+valor[i]+'\t'+estoque[i]+'\t'+carinho[i])
@@ -42,47 +43,51 @@ while(x < 1){
         }
         
         while(i < 1){
-            carinho[cod] = leia("coloque quanto vc quer? :")
+            carinho[cod] = leia("Coloque a quantidade requerida :")
             if(estoque[cod] >= carinho[cod] && carinho[cod] > 0){
                 estoque[cod] = estoque[i] - carinho[cod]
                 i++
             }
             else{
-            console.log("numero errado")   
+            console.log("Selecione apenas uma quantidade")   
             }
     }
     // joão continua ea
         pergunta = leia("deseja continuar S/N :")
         if(pergunta == "N" || pergunta == "n"){// se não
             console.log("tenha um bom dia");
-            break;
         }
         else{
-            console.log("\n\n\n")
+                    // Nota Fiscal - Última etapa
+        console.log(loja);
+        let imposto = 0.9 * total;
+        console.log("Imposto arrecadado: " + imposto);
+        console.log("Opcoes de pagamento: \n  1 para A VISTA (-10%) '\t' 2 para CARTAO (+10%) '\t' 3 para parcelar em 2 vezes (+15%)");
+        op = parseInt(leia("Digite a opção desejada: " ));
+        do{
+            op++;
+            if (op == 1) {
+            console.log("Selecionado: A vista");
+            total -= 0.1*(total);
+        } 
+            else if (op == 2){
+            console.log("Selecionado: 1x no cartao");
+            total += 0.1*(total);
+            }
+
+            else if (op == 3){
+            console.log("Selecionado: 2x no cartao")
+            total += 0.15*(total);
+            }
         }
+
+        while(op == 1 || op == 2 || op == 3);
+        console.log("Nota Fiscal");
+        console.log("COD \t PRODUTO \t VALOR \t QTD");
+        console.log(codigo[i] + produto[i] + valor[i] + (estoque[i] - carinho[codigo]));
+        console.log(" \n Obrigado, volte sempre!")
+            }
     }
+        // Carrinho de compra
 
-
-//carinho de compra
-// a tabelas não tao fucionando se tiver alguma duvida me fala ok
-// Programa Nota Fiscal
-
-console.log(loja);
-console.log("Imposto arrecadado: " + imposto);
-console.log("Opcoes de pagamento: \n  1 para A VISTA (-10%) '\t' 2 para CARTAO (+10%) '\t' 3 para parcelar em 2 vezes (+15%)");
-
-do{
-    op++;
-    if (op == 1) {
-    console.log("Selecionado: A vista");
-    total -= 0.1*(total);
-} 
-
-    else if (op == 2){
-    console.log("Selecionado: Com cartao")
-    total += 0.1*(total);
-    }
-
-}
-
-while(op == 1 || op == 2 || op == 3)*/
+// A tabelas não tão funcionando se tiver alguma dúvida me fala ok
