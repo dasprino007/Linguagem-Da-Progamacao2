da6d3c250358db055c82de079ed1b7aea72e7335
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-14 11:49:16 -0300

    at de java terminda/ pronto pra revisão

---
+++ OngLoja/script.js
@@ -0,0 +1,44 @@
+const leia = require("prompt-sync")()
+
+let codigo = ["COD1","COD2","COD3"]
+let servico = ["traduzir filme","traduzir livro","traduzir serie"]
+let VALORUNITARIODOACAO = [10.00,24.99,11.99]
+let movimento = [0,0,0]
+let loop = 0
+let resp
+let total = 0
+
+while(loop < 1){
+    console.log("codigo\tserviço\t\tvalor\tcarinho")
+    for(i = 0; i < codigo.length; i++){
+        console.log(codigo[i]+"\t"+servico[i]+"\t"+VALORUNITARIODOACAO[i]+"\t"+movimento[i])
+    }
+    
+    for(volta = 0; volta < 10; volta++){
+        resp = leia("coloque o codigo do item para comprar :").toUpperCase()
+        console.log(resp)
+        
+        for(i = 0; i < codigo.length; i++){
+            if(codigo[i] == resp){
+                movimento[i] += 1
+            }
+        }
+        
+        console.log("codigo\tserviço\t\tvalor\tcarinho")
+        for(i = 0; i < codigo.length; i++){
+            console.log(codigo[i]+"\t"+servico[i]+"\t"+VALORUNITARIODOACAO[i]+"\t"+movimento[i])
+        }
+    }
+    
+    for(i = 0; i < codigo.length; i++){
+        total += (movimento[i]*VALORUNITARIODOACAO[i])
+    }
+    
+    console.log("o valor total é de R$"+total)
+    movimento = [0,0,0]
+    resp = leia("deseja sair? :")
+    if(resp == "sim"){
+       loop++
+       console.log("muito obrigado") 
+    }
+}
