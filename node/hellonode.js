3e8b860254195b267ff0f20d12f3199111a03ad7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-22 11:41:36 -0300

    uns texto explicando melhor

--- node/hellonode.js
+++ node/hellonode.js
@@ -2,9 +2,12 @@ const http = require('http')
 
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
-    res.setHeader('Content-Type','Text/Plain');
-    res.end('Hello, Node.JS Server');
+    res.setHeader('Content-Type','Text/Plain'); // requisição do node
+    res.end('Hello, Node.JS Server'); // o que vai aparecer
 })
+
+// publicação da pagina no github pages
+// e o servidor em oracle
 
 const port = 3000;
 server.listen(port , () =>{
