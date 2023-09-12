35aa8e9325a2b883e09b8abd51a24e0a13d9d74c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-12 11:32:58 -0300

    exemplo promise

---
+++ node/exemplopromise.js
@@ -0,0 +1,11 @@
+function FalarDepoisDe(segundos,frase){
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            resolve(frase)
+        }, segundos * 1000)
+    })
+}
+
+FalarDepoisDe(3,"otima pergunta")
+.then(frase => frase.concat('?!?'))
+.then(outraFrase => console.log(outraFrase)) // recebe a frase concatiada da outra promise
